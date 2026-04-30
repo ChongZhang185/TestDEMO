932f6f0fabb78ad9d2d7d8cde974417ed717324a
chong.zhang <chong.zhang@endeavorstreaming.com>
2026-04-30 12:43:26 +0800

    review code

--- TestDemo/reader/RowItem.swift
+++ TestDemo/reader/RowItem.swift
@@ -1,212 +1,123 @@
 import Foundation
 
-//deviceState - time
-struct RowItem: Identifiable {
+private let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy/M/d HH:mm"
+    formatter.locale = Locale(identifier: "zh_CN")
+    return formatter
+}()
+
+class BaseRowItem: Identifiable {
     let id = UUID()
     let index: Int
     let timeString: String?
-    let deviceState: Int?
     let date: Date?
-    
-    init(index: Int, item: TelecomData) {
+
+    init(index: Int, timeString: String?) {
         self.index = index
-        self.timeString = item.time
+        self.timeString = timeString
+        self.date = dateFormatter.date(from: timeString ?? "") ?? Date()
+    }
+}
+
+//deviceState - time
+class RowItem: BaseRowItem {
+    let deviceState: Int?
+
+    init(index: Int, item: TelecomData) {
         self.deviceState = item.deviceState
-        
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy/M/d HH:mm"
-        formatter.locale = Locale(identifier: "zh_CN")
-        self.date = formatter.date(from: self.timeString ?? "") ?? Date()
+        super.init(index: index, timeString: item.time)
     }
 }
 
 // rsrp - time
-struct RowItem2: Identifiable {
-    let id = UUID()
-    let index: Int
-    let timeString: String?
-    let date: Date?
-    let rat1: String?
-    let rat2: String?
-    let rat3: String?
-    let rsrp1: [Int]?
-    let rsrp2: [Int]?
-    let rsrp3: [Int]?
-    let band1: String?
-    let band2: String?
-    let band3: String?
-    
+class RowItem2: BaseRowItem {
+    let rat1, rat2, rat3: String?
+    let rsrp1, rsrp2, rsrp3: [Int]?
+    let band1, band2, band3: String?
+
     init(index: Int, item: TelecomData) {
-        self.index = index
-        self.timeString = item.time
-        
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy/M/d HH:mm"
-        formatter.locale = Locale(identifier: "zh_CN")
-        self.date = formatter.date(from: self.timeString ?? "") ?? Date()
-        
         self.rat1 = item.rat1
         self.rat2 = item.rat2
         self.rat3 = item.rat3
-        
         self.rsrp1 = item.rsrp1
         self.rsrp2 = item.rsrp2
         self.rsrp3 = item.rsrp3
-        
         self.band1 = item.band1.trimmingCharacters(in: .whitespaces)
         self.band2 = item.band2.trimmingCharacters(in: .whitespaces)
         self.band3 = item.band3.trimmingCharacters(in: .whitespaces)
+        super.init(index: index, timeString: item.time)
     }
 }
 
 // TxPower - time
-struct RowItem3: Identifiable {
-    let id = UUID()
-    let index: Int
-    let timeString: String?
-    let date: Date?
+class RowItem3: BaseRowItem {
+    let band1, band2, band3: String?
+    let rat1, rat2, rat3: String?
+    var txPower1, txPower2, txPower3: [Double]?
 
-    let band1: String?
-    let band2: String?
-    let band3: String?
-    
-    let rat1: String?
-    let rat2: String?
-    let rat3: String?
-    
-    var txPower1: [Double]?
-    var txPower2: [Double]?
-    var txPower3: [Double]?
-    
-    
     init(index: Int, item: TelecomData) {
-        self.index = index
-        self.timeString = item.time
-        
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy/M/d HH:mm"
-        formatter.locale = Locale(identifier: "zh_CN")
-        self.date = formatter.date(from: self.timeString ?? "") ?? Date()
-        
         self.band1 = item.band1.trimmingCharacters(in: .whitespaces)
         self.band2 = item.band2.trimmingCharacters(in: .whitespaces)
         self.band3 = item.band3.trimmingCharacters(in: .whitespaces)
-        
         self.rat1 = item.rat1
         self.rat2 = item.rat2
         self.rat3 = item.rat3
-        
         self.txPower1 = item.txPower1
         self.txPower2 = item.txPower2
         self.txPower3 = item.txPower3
+        super.init(index: index, timeString: item.time)
     }
 }
 
 // Band - time
-struct RowItem4: Identifiable {
-    let id = UUID()
-    let index: Int
-    let timeString: String?
-    let date: Date?
+class RowItem4: BaseRowItem {
+    var band1, band2, band3: String?
 
-    var band1: String?
-    var band2: String?
-    var band3: String?
-  
     init(index: Int, item: TelecomData) {
-        self.index = index
-        self.timeString = item.time
-        
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy/M/d HH:mm"
-        formatter.locale = Locale(identifier: "zh_CN")
-        self.date = formatter.date(from: self.timeString ?? "") ?? Date()
         self.band1 = item.band1.trimmingCharacters(in: .whitespaces)
         self.band2 = item.band2.trimmingCharacters(in: .whitespaces)
         self.band3 = item.band3.trimmingCharacters(in: .whitespaces)
+        super.init(index: index, timeString: item.time)
     }
 }
 
 // UL_TPut - time
-struct RowItem5: Identifiable {
-    let id = UUID()
-    let index: Int
-    let timeString: String?
-    let date: Date?
-
-    let rat1: String?
-    let rat2: String?
-    let rat3: String?
-    
-    let ulTPut: Double?
-    let dlTPut: Double?
+class RowItem5: BaseRowItem {
+    let rat1, rat2, rat3: String?
+    let ulTPut, dlTPut: Double?
 
     init(index: Int, item: TelecomData) {
-        self.index = index
-        self.timeString = item.time
-        
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy/M/d HH:mm"
-        formatter.locale = Locale(identifier: "zh_CN")
-        self.date = formatter.date(from: self.timeString ?? "") ?? Date()
-        
         self.ulTPut = item.ulTPut
         self.dlTPut = item.dlTPut
-        
         self.rat1 = item.rat1
         self.rat2 = item.rat2
         self.rat3 = item.rat3
+        super.init(index: index, timeString: item.time)
     }
 }
 
-
 // UL_TPut - time
-struct RowItem6: Identifiable {
-    let id = UUID()
-    let index: Int
-    let timeString: String?
-    let date: Date?
-
-    let rat1: String?
-    let rat2: String?
-    let rat3: String?
-
-    let dlChannel1: String?
-    let dlChannel2: String?
-    let dlChannel3: String?
-    
-    let ulChannel1: String?
-    let ulChannel2: String?
-    
-    let band1: String?
-    let band2: String?
-    let band3: String?
-    
+class RowItem6: BaseRowItem {
+    let rat1, rat2, rat3: String?
+    let dlChannel1, dlChannel2, dlChannel3: String?
+    let ulChannel1, ulChannel2: String?
+    let band1, band2, band3: String?
     let dlChannelDes = "DL_Channel"
     let ulChannelDes = "UL_Channel"
-    
+
     init(index: Int, item: TelecomData) {
-        self.index = index
-        self.timeString = item.time
-
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy/M/d HH:mm"
-        formatter.locale = Locale(identifier: "zh_CN")
-        self.date = formatter.date(from: self.timeString ?? "") ?? Date()
-
         self.dlChannel1 = "\(item.dlChannel1)"
         self.dlChannel2 = "\(item.dlChannel2)"
         self.dlChannel3 = "\(item.dlChannel3)"
-
         self.ulChannel1 = "\(item.ulChannel1)"
         self.ulChannel2 = "\(item.ulChannel2)"
-
         self.rat1 = item.rat1
         self.rat2 = item.rat2
         self.rat3 = item.rat3
-        
         self.band1 = item.band1.trimmingCharacters(in: .whitespaces)
         self.band2 = item.band2.trimmingCharacters(in: .whitespaces)
         self.band3 = item.band3.trimmingCharacters(in: .whitespaces)
+        super.init(index: index, timeString: item.time)
     }
 }
